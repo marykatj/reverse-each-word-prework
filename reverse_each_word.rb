6d734f4c8d62afde28edf9ab49bde4f13115ea94
Mary-Kate James <marykatj@gmail.com>
2018-05-02 21:50:04 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,8 +2,8 @@ def reverse_each_word(sentence)
   array = sentence.split(" ", " ")
   new_array = []
   array.each do |word|
-    word.reverse!
-    new_array = new_array.push(word.reverse!)
+    reversed_word = word.reverse!
+    new_array = new_array.push(reversed_word)
   end
   return new_array.join(' , ')
 end
