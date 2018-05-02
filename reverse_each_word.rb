ef0008cdeb7c28aa234716a1b663808c5e1aa91a
Mary-Kate James <marykatj@gmail.com>
2018-05-02 22:09:59 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,15 @@
+def reverse_each_word(sentence)
+  array = sentence.split
+  array.each do |word|
+    word.reverse!
+  end
+  return array.join(" ")
+end
+
+def reverse_each_word(sentence)
+  array = sentence.split
+  array.collect do |word|
+    word.reverse!
+  end
+  return array.join(" ")
+end
