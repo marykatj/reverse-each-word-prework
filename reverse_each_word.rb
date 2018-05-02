6fa8bfc549343250a4d2b2e25dd02d00f1fa9dd0
Mary-Kate James <marykatj@gmail.com>
2018-05-02 20:59:00 +0000

    automatically backed up by learn

---
+++ reverse_each_word.rb
@@ -0,0 +1,17 @@
+def reverse_each_word(sentence)
+  new_string = ""
+  array = sentence.split(" , ")
+  array.each do |word|
+    sentence_string = new_string.push(word.reverse)
+  end
+  return sentence_string
+end
+
+def reverse_each_word(sentence)
+  new_string = ""
+  array = sentence.split(" , ")
+  array.collect do |word|
+    sentence_string = new_string.push(word.reverse)
+  end
+  return sentence_string
+end
